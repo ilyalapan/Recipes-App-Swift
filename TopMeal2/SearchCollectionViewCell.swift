3f3731c7d586ce3fc458182bc9bb10a0083498c2
Ilya Lapan <lapan.ilya@gmail.com>
2016-09-15 16:45:54 +0300

    Created the very basic collection view. Get more practise with collection views before continuing, there are some problems

---
+++ TopMeal2/SearchCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SearchCollectionViewCell.swift
+//  TopMeal2
+//
+//  Created by Ilya Lapan on 15/09/2016.
+//  Copyright © 2016 ilyaseva. All rights reserved.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+    
+}
