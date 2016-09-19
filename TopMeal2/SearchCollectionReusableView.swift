e0ff4850adf0ad0c48727d05031566fb55066e32
Ilya Lapan <lapan.ilya@gmail.com>
2016-09-19 15:26:38 +0300

    Fixed minor bugs and extended implementation of sliding view. Added header view with time slider top the top of Search View.

---
+++ TopMeal2/SearchCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  SearchCollectionReusableView.swift
+//  TopMeal2
+//
+//  Created by Ilya Lapan on 16/09/2016.
+//  Copyright © 2016 ilyaseva. All rights reserved.
+//
+
+import UIKit
+
+class SearchCollectionReusableView: UICollectionReusableView {
+        
+}
